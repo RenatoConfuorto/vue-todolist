4a596a776dbbdcde99a3e961ff3c3163bf62c0d7
RenatoConfuorto <renato.confuorto@outlook.it>
2022-05-09 18:09:59 +0200

    Milestone 1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const app = new Vue({
+  el: '#root',
+  data: {
+    toDo:[
+      {
+        text: "Fare l'esercizio",
+        done: false
+      },
+      {
+        text: "Dare da mangiare ai gatti",
+        done: true
+      },
+      {
+        text: "Riguardare la lezione",
+        done: false
+      },
+      {
+        text: "Fare la spesa",
+        done: false
+      }
+    ]
+  },
+  methods:{
+
+  }
+});
